1062fefb588f0efd98dd026ea6ff72b711e7e200
sandilokaa <sandyloka790@gmail.com>
2025-04-02 23:54:13 +0700

    docs: add footer components

---
+++ src/components/common/footer/Footer.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Footer() {
+    return (
+        <div className="p-5 h-auto bg-white">
+            <div className="flex justify-center">
+                <div className="flex items-center">
+                    <p className="text-[10px]">© 2025 Merpati Wahana Raya. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
