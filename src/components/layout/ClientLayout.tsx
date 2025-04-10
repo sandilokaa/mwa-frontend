d06ea812468132d75c7d1e52e865d87fd54cf070
sandilokaa <sandyloka790@gmail.com>
2025-04-10 15:58:13 +0700

    docs: set provider

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -5,25 +5,29 @@ import Sidebar from "@/components/sidebar/Sidebar";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/common/footer/Footer";
 import { ProductFilterProvider } from "@/context/ProductFilterContext";
+import { store } from "@/store/store";
+import { Provider } from "react-redux";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
     const isAuthPage = pathname.startsWith("/login");
 
-    if (isAuthPage) {
-        return <div className="h-screen flex">{children}</div>;
-    }
-
     return (
-        <ProductFilterProvider>
-            <div className="flex h-screen overflow-hidden">
-                    <Sidebar />
-                <div className="flex-1 flex flex-col h-screen">
-                    <Navbar />
-                <div className="flex-1 overflow-auto p-5">{children}</div>
-                    <Footer />
+        <Provider store={store}>
+            {isAuthPage ? (
+                <div className="h-screen flex">{children}</div>
+            ) : (
+            <ProductFilterProvider>
+                <div className="flex h-screen overflow-hidden">
+                        <Sidebar />
+                    <div className="flex-1 flex flex-col h-screen">
+                        <Navbar />
+                    <div className="flex-1 overflow-auto p-5">{children}</div>
+                        <Footer />
+                    </div>
                 </div>
-            </div>
-        </ProductFilterProvider>
+            </ProductFilterProvider>
+            )}
+        </Provider>
     );
 }
