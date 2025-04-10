73a4c650b0092c6b5e9ecd99c5d56321ebef2878
sandilokaa <sandyloka790@gmail.com>
2025-04-10 16:29:45 +0700

    docs: add snackbar

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { Provider } from "react-redux";
+import { SnackbarProvider } from "notistack";
 import Sidebar from "@/components/sidebar/Sidebar";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/common/footer/Footer";
 import { ProductFilterProvider } from "@/context/ProductFilterContext";
 import { store } from "@/store/store";
-import { Provider } from "react-redux";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
@@ -14,20 +15,25 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 
     return (
         <Provider store={store}>
-            {isAuthPage ? (
-                <div className="h-screen flex">{children}</div>
-            ) : (
-            <ProductFilterProvider>
-                <div className="flex h-screen overflow-hidden">
-                        <Sidebar />
-                    <div className="flex-1 flex flex-col h-screen">
-                        <Navbar />
-                    <div className="flex-1 overflow-auto p-5">{children}</div>
-                        <Footer />
+            <SnackbarProvider 
+                maxSnack={3}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            >
+                {isAuthPage ? (
+                    <div className="h-screen flex">{children}</div>
+                ) : (
+                <ProductFilterProvider>
+                    <div className="flex h-screen overflow-hidden">
+                            <Sidebar />
+                        <div className="flex-1 flex flex-col h-screen">
+                            <Navbar />
+                        <div className="flex-1 overflow-auto p-5">{children}</div>
+                            <Footer />
+                        </div>
                     </div>
-                </div>
-            </ProductFilterProvider>
-            )}
+                </ProductFilterProvider>
+                )}
+            </SnackbarProvider>
         </Provider>
     );
 }
