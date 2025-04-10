2e7f92ba6fb7978c37530d6950553c73e494fb02
sandilokaa <sandyloka790@gmail.com>
2025-04-10 16:30:11 +0700

    docs: add onChange props

--- src/components/common/input/InputForm.tsx
+++ src/components/common/input/InputForm.tsx
@@ -4,9 +4,10 @@ interface InputFormProps {
     label: string;
     type?: string;
     placeholder?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputForm = forwardRef<HTMLInputElement, InputFormProps>(({ label, type = "text", placeholder }, ref) => {
+const InputForm = forwardRef<HTMLInputElement, InputFormProps>(({ label, type = "text", placeholder, onChange }, ref) => {
     return (
         <div className="flex flex-col gap-1">
             <label className="text-sm font-medium">{label}</label>
@@ -14,6 +15,7 @@ const InputForm = forwardRef<HTMLInputElement, InputFormProps>(({ label, type = 
                 ref={ref}
                 type={type}
                 placeholder={placeholder}
+                onChange={onChange}
                 className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-1 focus:ring-gray-300 placeholder:text-sm h-[45px]"
             />
         </div>
