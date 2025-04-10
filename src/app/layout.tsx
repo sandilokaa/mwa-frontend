2e4456af65bc3c23ceb2048910dcfcc19d1eca82
sandilokaa <sandyloka790@gmail.com>
2025-04-10 15:58:06 +0700

    docs: tidy up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${montserrat.variable} antialiased`}>
-        <ClientLayout>{children}</ClientLayout>
+          <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
