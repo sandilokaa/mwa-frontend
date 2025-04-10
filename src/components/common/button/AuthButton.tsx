a01a0be7e1ffd0a7ee603d297329fb8e7ebcac34
sandilokaa <sandyloka790@gmail.com>
2025-04-10 15:58:45 +0700

    docs: add props

--- src/components/common/button/AuthButton.tsx
+++ src/components/common/button/AuthButton.tsx
@@ -2,11 +2,15 @@
 
 interface AuthButtonProps {
     buttonText: string;
+    onClick?: () => void;
 }
 
-const AuthButton: React.FC<AuthButtonProps> = ({ buttonText }) => {
+const AuthButton: React.FC<AuthButtonProps> = ({ buttonText, onClick }) => {
     return (
-        <div className="flex justify-center w-full items-center bg-[#144C68] text-white py-2 px-4 rounded-md hover:bg-[#0E3549] transition-colors cursor-pointer h-[45px]">
+        <div 
+            onClick={onClick} 
+            className="flex justify-center w-full items-center bg-[#144C68] text-white py-2 px-4 rounded-md hover:bg-[#0E3549] transition-colors cursor-pointer h-[45px]"
+        >
             <p className="font-medium">{buttonText}</p>
         </div>
     );
