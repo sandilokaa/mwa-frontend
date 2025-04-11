53f4bdd7452cc59316af0ae77d4b901c875a462c
sandilokaa <sandyloka790@gmail.com>
2025-04-11 14:16:04 +0700

    docs: set auth init

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -8,32 +8,34 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/common/footer/Footer";
 import { ProductFilterProvider } from "@/context/ProductFilterContext";
 import { store } from "@/store/store";
+import AuthInit from "../common/auth/AuthInit";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
     const isAuthPage = pathname.startsWith("/login");
 
     return (
-        <Provider store={store}>
-            <SnackbarProvider 
-                maxSnack={3}
-                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            >
+        <SnackbarProvider 
+            maxSnack={3}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+            <Provider store={store}>
                 {isAuthPage ? (
                     <div className="h-screen flex">{children}</div>
                 ) : (
-                <ProductFilterProvider>
-                    <div className="flex h-screen overflow-hidden">
-                            <Sidebar />
-                        <div className="flex-1 flex flex-col h-screen">
-                            <Navbar />
-                        <div className="flex-1 overflow-auto p-5">{children}</div>
-                            <Footer />
+                    <ProductFilterProvider>
+                        <AuthInit/>
+                        <div className="flex h-screen overflow-hidden">
+                                <Sidebar />
+                            <div className="flex-1 flex flex-col h-screen">
+                                <Navbar />
+                            <div className="flex-1 overflow-auto p-5">{children}</div>
+                                <Footer />
+                            </div>
                         </div>
-                    </div>
-                </ProductFilterProvider>
+                    </ProductFilterProvider>
                 )}
-            </SnackbarProvider>
-        </Provider>
+            </Provider>
+        </SnackbarProvider>
     );
 }
