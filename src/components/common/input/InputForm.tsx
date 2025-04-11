2dcca582dd7ee2cf1cf6ea30f321d06f55d2390b
sandilokaa <sandyloka790@gmail.com>
2025-04-11 14:36:34 +0700

    docs: add see/hide password

--- src/components/common/input/InputForm.tsx
+++ src/components/common/input/InputForm.tsx
@@ -1,4 +1,5 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 interface InputFormProps {
     label: string;
@@ -8,16 +9,32 @@ interface InputFormProps {
 }
 
 const InputForm = forwardRef<HTMLInputElement, InputFormProps>(({ label, type = "text", placeholder, onChange }, ref) => {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const isPassword = type === "password";
+    const inputType = isPassword ? (showPassword ? "text" : "password") : type;
+
     return (
         <div className="flex flex-col gap-1">
             <label className="text-sm font-medium">{label}</label>
-            <input
-                ref={ref}
-                type={type}
-                placeholder={placeholder}
-                onChange={onChange}
-                className="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-1 focus:ring-gray-300 placeholder:text-sm h-[45px]"
-            />
+            <div className="relative">
+                <input
+                    ref={ref}
+                    type={inputType}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-1 focus:ring-gray-300 placeholder:text-sm h-[45px] text-sm"
+                />
+                {isPassword && (
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword((prev) => !prev)}
+                        className="absolute right-5 top-1/2 -translate-y-1/2 text-gray-500 focus:outline-none cursor-pointer"
+                    >
+                        {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
+                    </button>
+                )}
+            </div>
         </div>
     );
 });
