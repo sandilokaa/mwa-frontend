66abfa7bd55270605f882eb88374e03ca2b5ed04
sandilokaa <sandyloka790@gmail.com>
2025-04-16 23:03:43 +0700

    docs: tidy up

--- src/components/common/auth/AuthInit.tsx
+++ src/components/common/auth/AuthInit.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useAppDispatch } from "@/store/hooks";
-import { fetchCurrentUser } from "@/store/slice/authSlice";
+import { fetchCurrentUser } from "@/store/slice/auth/authSlice";
 
 export default function AuthInit() {
     const dispatch = useAppDispatch();
