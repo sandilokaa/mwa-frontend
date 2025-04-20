da121e95839d7fb6ec6bab2ad5602ac10a1e0a2d
sandilokaa <sandyloka790@gmail.com>
2025-04-20 19:52:57 +0700

    docs: add new file

---
+++ src/components/common/modal/ProgressUpdate.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+
+const ProgressUpdate: React.FC = () => {
+
+    return (
+        <div>
+            <p>Haloo ini modal progress</p>
+        </div>
+    );
+};
+
+export default ProgressUpdate;
