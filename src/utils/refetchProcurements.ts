7f4f65b6dd364b6d8df593b8fa6fed3cbe50b47c
sandilokaa <sandyloka790@gmail.com>
2025-04-24 22:10:09 +0700

    docs: tidy up

--- src/utils/refetchProcurements.ts
+++ src/utils/refetchProcurements.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { fetchSummaryProcurement } from "@/store/slice/procurement/getAllSlice";
-import { fetchFilteredProcurement, fetchAllProcurements } from "@/store/slice/procurement/getAllSlice";
+import { fetchFilteredProcurement, fetchAllProcurements, fetchSummaryProcurement } from "@/store/slice/procurement/getAllSlice";
 
 export const refetchProcurements = (dispatch: any, productId: any, search: any, page = 1) => {
     dispatch(fetchSummaryProcurement({ productId }));
