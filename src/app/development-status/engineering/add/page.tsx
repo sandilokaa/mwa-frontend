e21804867ea02be5445820187aa33e4704eae6c9
sandilokaa <sandyloka790@gmail.com>
2025-04-29 23:33:41 +0700

    docs: preparing engineering page

--- src/app/development-status/engineering/add/page.tsx
+++ src/app/development-status/engineering/add/page.tsx
@@ -3,6 +3,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import InputForm from "@/components/common/input/InputForm";
+import DropdownProductForm from "@/components/common/dropdown/DropdownProductForm";
+import DropdownString from "@/components/common/dropdown/DropdownString";
+import TextAreaForm from "@/components/common/input/TextAreaForm";
+import FileInputForm from "@/components/common/input/FileInputForm";
+import SubmitButton from "@/components/common/button/SubmitButton";
+
 export default function AddData() {
 
     return (
@@ -11,10 +18,96 @@ export default function AddData() {
                 <Link className="cursor-pointer" href="/development-status/engineering">
                     <Image src="/images/icon/chevron-down.svg" width={24} height={24} alt="Back Icon"/>
                 </Link>
-                <p className="font-bold">Add Engineering</p>
+                <p className="font-bold">Add Designed Engineering</p>
             </div>
             <div className="mt-5 bg-white w-full rounded-[10px] p-5">
-                <p className="text-sm font-bold">Engineering Form</p>
+                <p className="text-sm font-bold">Designed Engineering Form</p>
+                <div className="flex flex-col gap-y-5 mt-5">
+                    <div className="flex flex-col gap-y-4">
+                        <div className="grid grid-cols-3 gap-4">
+                            <InputForm
+                                label="Part Name *"
+                                placeholder="Example: Chassis Assy"
+                            />
+                            <InputForm
+                                label="Part Number *"
+                                placeholder="Example: Chassis Assy"
+                            />
+                            <InputForm
+                                label="Drawing Number *"
+                                placeholder="Example: Chassis Assy"
+                            />
+                        </div>
+                        <div className="grid grid-cols-3 gap-4">
+                            <DropdownString
+                                label="Status 3D *"
+                                options={["Mechanical Engineering"]}
+                                onSelect={() => ""}
+                            />
+                            <DropdownString
+                                label="Status 2D *"
+                                options={["Mechanical Engineering"]}
+                                onSelect={() => ""}
+                            />
+                            <DropdownString
+                                label="Status DXF *"
+                                options={["Mechanical Engineering"]}
+                                onSelect={() => ""}
+                            />
+                        </div>
+                        <div className="grid grid-cols-3 gap-4">
+                            <DropdownProductForm
+                                label="Product *"
+                                options={[]}
+                                onSelect={() => ""}
+                            />
+                            <DropdownString
+                                label="PIC 3D *"
+                                options={["Mechanical Engineering"]}
+                                onSelect={() => ""}
+                            />
+                            <DropdownString
+                                label="PIC 2D & DXF *"
+                                options={["Mechanical Engineering"]}
+                                onSelect={() => ""}
+                            />
+                        </div>
+                        <div className="grid grid-cols-3 gap-4">
+                            <InputForm
+                                label="Price *"
+                                placeholder="Example: Chassis Assy"
+                            />
+                            <InputForm
+                                label="Quantity *"
+                                placeholder="Example: Chassis Assy"
+                            />
+                            <InputForm
+                                label="Total Price *"
+                                placeholder="Example: Chassis Assy"
+                                disable
+                            />
+                        </div>
+                        <div className="grid grid-cols-1">
+                            <TextAreaForm
+                                label="Remarks *"
+                                placeholder="Example: Describe the remark"
+                                rows={3}
+                            />
+                        </div>
+                        <div className="grid grid-cols-1">
+                            <FileInputForm
+                                label="Upload Documents *"
+                                acceptFile=".pdf"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex justify-end">
+                        <SubmitButton
+                            onClick={() => ""}
+                            buttonText="Add Production"
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/app/development-status/engineering/page.tsx
+++ src/app/development-status/engineering/page.tsx
@@ -3,18 +3,54 @@
 import Link from "next/link";
 
 import AddButton from "@/components/common/button/AddButton";
+import SearchInput from "@/components/common/input/SearchInput";
 
 export default function ShowData() {
     return (
         <div>
             <div className="flex flex-col gap-y-5">
-                <p className="font-bold">Development Status Engineering</p>
+                <p className="font-bold">Designed Engineering</p>
                 <div className="flex justify-between">
                     <Link href="/development-status/engineering/add">
                         <AddButton
-                            buttonText="Add Engineering"
+                            buttonText="Add Designed Engineering"
                         />
                     </Link>
+                    <SearchInput
+                        value={""}
+                        onChange={() => "setSearch(e.target.value)"}
+                    />
+                </div>
+            </div>
+            <div className="mt-5">
+                <div className="flex gap-4 w-full">
+                    <div className="flex flex-col justify-between w-3/4 bg-white p-5 rounded-[10px]">
+                        <h2 className="font-bold text-sm">Cumulative Designed Engineering Performance</h2>
+                        <div className="flex justify-center">
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-1/3 h-full bg-white p-5 rounded-[10px]">
+                        <h2 className="font-bold text-sm">Designed Engineering Status Overview</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5">
+            <div className="overflow-x-auto rounded-lg bg-white p-[10px]">
+                    <table className="w-full border-collapse">
+                        <thead className="border-b border-[#F5F5F5]">
+                            <tr className="text-sm font-bold text-center">
+                                <th className="py-5 px-4 text-left">No</th>
+                                <th className="py-5 px-4 text-left min-w-[200px]">Part Name</th>
+                                <th className="py-5 px-4 min-w-[170px]">Drawing Number</th>
+                                <th className="py-5 px-4 min-w-[170px]">Part Number</th>
+                                <th className="py-5 px-4 min-w-[170px]">Remarks</th>
+                                <th className="py-5 px-4 min-w-[150px]">Status</th>
+                                <th className="py-5 px-4 min-w-[150px]">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
