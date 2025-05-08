5297e087509ac11eaf55d8e2b7e3cfd33e6bf0b5
sandilokaa <sandyloka790@gmail.com>
2025-05-08 14:49:37 +0700

    docs: tidy up

--- src/components/common/modal/ConfirmDialog.tsx
+++ src/components/common/modal/ConfirmDialog.tsx
@@ -28,16 +28,16 @@ const ConfirmDialog: React.FC<ConfirmDialogProps> = ({
             <div className="bg-white p-6 rounded shadow-md min-w-[300px]">
                 <h3 className="font-bold text-lg mb-2">{title}</h3>
                 <p className="mb-5 text-sm text-black">{message}</p>
-                <div className="flex justify-end gap-3">
+                <div className="flex justify-end gap-3 font-medium">
                     <button
                         onClick={onClose}
-                        className="px-4 py-2 bg-gray-300 rounded cursor-pointer"
+                        className="px-4 py-2 text-sm bg-gray-300 rounded cursor-pointer"
                     >
                         {cancelText}
                     </button>
                     <button
                         onClick={onConfirm}
-                        className="px-4 py-2 bg-[#D62C35] text-white rounded cursor-pointer"
+                        className="px-4 py-2 text-sm bg-[#D62C35] text-white rounded cursor-pointer"
                     >
                         {confirmText}
                     </button>
